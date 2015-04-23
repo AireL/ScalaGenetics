137ded9d1f7b78be616193540c07e20e04bd2def
AireL <tomma2008@hotmail.com>
2015-04-23 17:04:54 +0100

    Update MutatorOp.scala
    Tibor-invoked-idea

--- src/main/scala/org/aire/mutation/MutatorOp.scala
+++ src/main/scala/org/aire/mutation/MutatorOp.scala
@@ -7,6 +7,6 @@ import scala.util.Random
 trait MutatorOp  {
   trait MutatorMagnet {
     type Result[_]
-    def apply[K[_]]()(implicit ev: K[_] =:= Result[_]): Result[_]
+    def apply() : Result[_]
   }
 }
